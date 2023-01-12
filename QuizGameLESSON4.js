faf4bcba3913ab839f4ad43c1c5d0a4cfd2ee87e
EdwART <122360856+EdwARTSs@users.noreply.github.com>
2023-01-12 22:47:16 +0800

    Add files via upload

---
+++ QuizGameLESSON4.js
@@ -0,0 +1,94 @@
+/*QUESTIONS FOR THE QUIZ LESSON 4*/
+
+const questions = [
+    {
+		question: 'This HTML tag is similar to the bold tag, defines bold text',
+		answers: [
+			{text: '<strong>', correct: true},
+			{text: '<stronger>', correct: false},
+			{text: '<em>', correct: false},
+			{text: '<bol>', correct: false}
+		]
+	},
+    {
+		question: 'This element defines a part of text to be in italic, similar to the italic tag',
+		answers: [
+			{text: '<i>', correct: false},
+			{text: '<em>', correct: true},
+			{text: '<tilt>', correct: false},
+			{text: '<italize>', correct: false}
+		]
+	},
+    {
+		question: 'This element defines text that should be marked or highlighted',
+		answers: [
+			{text: '<marked>', correct: false},
+			{text: '<hightlight>', correct: false},
+			{text: '<mark>', correct: true},
+			{text: '<marking>', correct: false}
+		]
+	},
+    {
+		question: 'This element defines smaller text',
+		answers: [
+			{text: '<little>', correct: false},
+			{text: '<size small = "">', correct: false},
+			{text: '<tiny>', correct: false},
+			{text: '<small>', correct: true}
+		]
+	},
+    {
+		question: 'This element defines text that has been deleted from a document',
+		answers: [
+			{text: '<del>', correct: true},
+			{text: '<delete>', correct: false},
+			{text: '<backspace>', correct: false},
+			{text: '<trash>', correct: false}
+		]
+	},
+    {
+		question: 'This element defines a text that has been inserted into a document',
+		answers: [
+			{text: '<insert_this>', correct: false},
+			{text: '<ins>', correct: true},
+			{text: '<inserting>', correct: false},
+			{text: '<i>', correct: false}
+		]
+	},
+    {
+		question: 'This element defines subscript text',
+		answers: [
+			{text: '<script sub = "">', correct: false},
+			{text: '<subscript>', correct: false},
+			{text: '<sub>', correct: true},
+			{text: '<sub.script>', correct: false}
+		]
+	},
+    {
+		question: 'This element defines superscript text',
+		answers: [
+			{text: '<sup.script>', correct: false},
+			{text: '<script sup ="">', correct: false},
+			{text: '<superscript>', correct: false},
+			{text: '<sup>', correct: true}
+		]
+	},
+    {
+		question: 'This tag defines a devision or a section in an HTML document, this is also known as a container',
+		answers: [
+			{text: '<div>', correct: true},
+			{text: '<divider>', correct: false},
+			{text: '<contain.this>', correct: false},
+			{text: '<section div ="">', correct: false}
+		]
+	},
+    {
+		question: 'This tag is an inline container used to mark up a part of a text or a part of a document',
+		answers: [
+			{text: '<class>', correct: false},
+			{text: '<span>', correct: true},
+			{text: '<spanning>', correct: false},
+			{text: '<getElementBySpan>', correct: false}
+		]
+	}
+]
